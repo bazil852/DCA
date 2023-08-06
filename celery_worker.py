ccf6ebbac96ee2253d335a7b497849843009c023
Bazil Sajjad <bazil@Bazils-MacBook-Air.local>
2023-08-07 03:16:53 +0500

    New baktest

--- celery_worker.py
+++ celery_worker.py
@@ -5,13 +5,13 @@ import os
 import sys
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-app = Celery("FAST", broker="pyamqp://guest@localhost//")
+# app = Celery("FAST", broker="pyamqp://guest@localhost//")
 
 # amqps://ouifibyf:V541bgbpT1dP6dQnOhHRy1yWJ97vdqmO@stingray.rmq.cloudamqp.com/ouifibyf
 # amqps://ouifibyf:V541bgbpT1dP6dQnOhHRy1yWJ97vdqmO@stingray.rmq.cloudamqp.com/ouifibyf
 
 
-# app = Celery("FAST", broker=os.environ['CLOUDAMQP_URL'])
+app = Celery("FAST", broker=os.environ['CLOUDAMQP_URL'])
 
 @app.task
 def run_bot_instance( strategy_id):
